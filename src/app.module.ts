9e61184ff546261ab9c5190f944bd783a6f068e1
Ayush Magar <7magarayush@gmail.com>
2026-05-14 21:57:43 +0545

    feat: initial project setup with TypeORM and PostgreSQL connection

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'], 
+        synchronize: true,
+      }),
+    }),
+  ],
+})
+export class AppModule {}
